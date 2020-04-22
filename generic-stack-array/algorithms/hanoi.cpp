193ad0386719774c3bee9bb084dcbf9ebab23fa4
RiffLord <bruno.pezer@hotmail.com>
2020-04-22 15:27:26 +0200

    Finished array stack & added pointer implemented stack.

--- generic-stack-array/algorithms/hanoi.cpp
+++ generic-stack-array/algorithms/hanoi.cpp
@@ -1,71 +1,22 @@
-//  Recursively calculates the solution to a Tower of Hanoi
-//  game with the number of disks provided by the user.
-
-/*
-    Transforming a recursive procedure to an iterative one requires
-        - the creation of a stack after the initial begin
-        - the substitution of each recursive call with a series of
-          instructions which:
-          - save the values of the local parameters and the label of               the instruction following the recursive call into the stack
-          - assign the appropriate values to the parameters
-            - the code relating to the sequence of instruction for                   this call is executed
-          - jump to the instruction following the stack creation
-        - the introduction before the final end of instructions which,
-          in case the stack is not empty, pop from it the stored                 values and jump to the instruction whose label is equal to             the return point
- */
-
- /*
-    Type definitions:
-    hanoi: structured type with the components
-        numberOfDisks: integer
-        pole:          origin, support, destination
-        return:        integer
-
-    move(int n, pole origin, pole destination, pole support)
-        stack.init()
-    1:    if (n == 1)
-            moveSrcToDest
-            goto 3
-        hanoi.numberOfDisks = n
-        hanoi.origin = origin
-        hanoi.support = support
-        hanoi.destination = destination
-        hanoi.return = 2
-        stack.push(hanoi)
-        n = n - 1
-        temp = destination
-        destination = support
-        support = temp
-        goto 1
-    2:  moveSrcToDest()
-            hanoi.numberOfDisks = n
-            hanoi.source = source
-            hanoi.support = support
-            hanoi.destination = destination
-            hanoi.return = 3
-            stack.push(hanoi)
-            n = n - 1
-            temp = source
-            source = support
-            support = temp
-            goto 1
-    3:  if !stack.isEmpty()
-            hanoi = stack.read()
-            stack.pop()
-            n = hanoi.numberOfDisks
-            source = hanoi.source
-            destination = hanoi.destination
-            support = hanoi.support
-            case hanoi.return of
-                2: goto 2
-                3: goto 3
-  */
+//  Uses a stack to iteratively calculate the solution to a Tower 
+//  of Hanoi game with the number of disks provided by the user.
 #include <iostream>
 #include <sstream>
+#include "stack.h"
 
 using namespace std;
 
 enum pole { origin, support, destination };
+
+//  The state of the game is represented by a
+//  struct which will be stored in the stack
+struct hanoi {
+    int disks;
+    pole org;
+    pole sup;
+    pole dest;
+    int returnValue;
+};
 
 void printPole(pole p) {
     if (p == origin) cout << " origin ";
@@ -73,7 +24,11 @@ void printPole(pole p) {
     else cout << " destination ";
 }
 
-void moveOriginToDest(pole o, pole d) {
+//  Represents the movement of a disk from one pole
+//  to another, incrementing the number of moves when
+//  called.
+void moveOriginToDest(pole o, pole d, int& moves) {
+    moves++;
     cout << "move a disk from ";
     printPole(o);
     cout << "to";
@@ -82,15 +37,56 @@ void moveOriginToDest(pole o, pole d) {
 }
 
 void move(int n, pole o, pole s, pole d, int& m) {
-    if (n == 1) {
-        moveOriginToDest(o, d);
-        m++;
-    } else {
-        move(n - 1, o, d, s, m);
-        moveOriginToDest(o, d);
-        m++;
-        move(n - 1, s, o, d, m);
-    }
+    stack<hanoi> steps; //  Each step of the game is stored in a stack
+    pole temp;
+    hanoi gameState;
+
+    step1:
+        if (n == 1) {
+            moveOriginToDest(o, d, m);
+            goto step3;
+        }
+        gameState.disks = n;
+        gameState.org = o;
+        gameState.sup = s;
+        gameState.dest = d;
+        gameState.returnValue = 2;
+        steps.push(gameState);
+        n--;
+        temp = d;
+        d = s;
+        s = temp;
+        goto step1;
+
+    step2:
+        moveOriginToDest(o, d, m);
+        gameState.disks = n;
+        gameState.org = o;
+        gameState.sup = s;
+        gameState.dest = d;
+        gameState.returnValue = 3;
+        steps.push(gameState);
+        n--;
+        temp = o;
+        o = s;
+        s = temp;
+        goto step1;
+
+    step3:
+        if (!steps.isEmpty()) {
+            hanoi sp = steps.read();
+            steps.pop();
+            n = sp.disks;
+            o = sp.org;
+            d = sp.dest;
+            s = sp.sup;
+            switch (sp.returnValue) {
+                case 2:
+                    goto step2;
+                case 3:
+                    goto step3;
+            }
+        }
 }
 
 int main(int argc, char* argv[]) {
---
+++ stack-pointer/stack.h
@@ -0,0 +1,97 @@
+//  Pointer implementation of a stack.
+#ifndef STACK_H
+#define STACK_H
+
+#include <iostream>
+#include <assert.h>
+
+template <class T>
+class stack;
+
+template <class T>
+class node {
+    typedef T data_type;
+    friend class stack<T>;
+public:
+    data_type getValue() const { return value; }
+    node<T>* next;
+private:
+    data_type value;
+};
+
+template <class T>
+class stack {
+    typedef typename node<T>::data_type data_type;
+
+    friend std::ostream& operator<<(std::ostream& out, const stack<T>& stk) {
+        node<T>* temp = stk.head;
+        while (temp != nullptr) {
+            out << temp->getValue() << ' ';
+            temp = temp->next;
+        }
+
+        out << std::endl;
+
+        return out;
+    }
+
+public:
+
+    stack();
+    ~stack();
+
+    void init();
+    bool isEmpty() const;
+    data_type read() const;
+    void push(data_type);
+    void pop();
+    
+    int size() const;
+
+private:
+    node<T>* head;
+    int length;
+};
+
+template<class T>
+stack<T>::stack() {
+    head = nullptr;
+    length = 0;
+}
+
+template<class T>
+stack<T>::~stack() {
+    while (!isEmpty()) pop();
+    delete head;
+}
+
+template<class T>
+bool stack<T>::isEmpty() const { return head == nullptr; }
+
+template<class T>
+void stack<T>::push(data_type d) {
+    node<T>* temp = new node<T>;
+    temp->value = d;
+    temp->next = head;
+    head = temp;
+    length++;
+}
+
+template<class T>
+void stack<T>::pop() {
+    node<T>* temp = head;
+    head = head->next;
+    length--;
+    delete temp;
+}
+
+template<class T>
+typename stack<T>::data_type stack<T>::read() const { 
+    assert(!isEmpty());
+    return head->value; 
+}
+
+template<class T>
+int stack<T>::size() const { return length; }
+
+#endif // STACK_H
